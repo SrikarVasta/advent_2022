4ce1cacfc05f49e0db9503baedb825f8d5f7e20c
srikarvasta <vasta.srikar@gmail.com>
2022-12-04 09:26:11 +0200

    Solves advent_4

--- advent_4.py
+++ advent_4.py
@@ -1,2 +1,25 @@
-with open('advent_2.in') as file:
+from functools import reduce
+
+with open('advent_4.in') as file:
         data = [i for i in file.read().strip().split('\n')]
+
+def get_counting_string(min_num,max_num):
+    return reduce(lambda a, b: a +''+ b, map(str, range(min_num, max_num + 1)))
+
+overlaps = 0
+exploded_list = []
+
+for item in data:
+    split_item =item.split(",")
+    left_a, left_b = split_item[0].split("-")
+    right_a, right_b = split_item[1].split("-")
+   
+    left_string = get_counting_string(int(left_a),int(left_b))
+    right_string = get_counting_string(int(right_a),int(right_b))
+    exploded_list.append((left_string,right_string))
+
+    if left_string in right_string or right_string in left_string:
+        overlaps += 1
+        
+print(overlaps)
+# print(exploded_list)
